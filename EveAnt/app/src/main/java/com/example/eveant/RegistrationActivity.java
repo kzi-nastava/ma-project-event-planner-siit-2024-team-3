10bfb7d5685a9e5b4677972c325f1d8671238b09
kuridza1 <131389372+kuridza1@users.noreply.github.com>
2024-11-05 14:18:04 +0100

    [UPDATE] - Enhancing registration UI

--- EveAnt/app/src/main/java/com/example/eveant/RegistrationActivity.java
+++ EveAnt/app/src/main/java/com/example/eveant/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.eveant;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -16,6 +19,29 @@ public class RegistrationActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registration);
 
+        ToggleButton organizerButton = findViewById(R.id.organizerButton);
+        ToggleButton providerButton = findViewById(R.id.providerButton);
 
+        organizerButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    providerButton.setChecked(false);
+                    organizerButton.setTextColor(getColor(R.color.white));
+                    providerButton.setTextColor(getColor(R.color.black));
+                }
+            }
+        });
+
+        providerButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    organizerButton.setChecked(false);
+                    organizerButton.setTextColor(getColor(R.color.black));
+                    providerButton.setTextColor(getColor(R.color.white));
+                }
+            }
+        });
     }
 }
