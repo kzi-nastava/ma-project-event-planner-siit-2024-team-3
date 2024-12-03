5d2ad2a4222f87e739191ab6762920f87fecb84b
mionalukic <155630561+mionalukic@users.noreply.github.com>
2024-12-03 16:50:00 +0100

    [ADD] gui part for service/product details

---
+++ EveAnt/app/src/main/java/com/example/eveant/ServiceDetails.java
@@ -0,0 +1,13 @@
+package com.example.eveant;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ServiceDetails extends Fragment {
+
+}
