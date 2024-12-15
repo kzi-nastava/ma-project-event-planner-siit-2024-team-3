ec22090ed998722fc44cc698949f0819ea5f9248
kuridza1 <131389372+kuridza1@users.noreply.github.com>
2024-12-15 21:41:33 +0100

    [UPDATE] - Activation fragment requests

--- EveAnt/app/src/main/java/com/example/eveant/user/registration/ActivationFragment.java
+++ EveAnt/app/src/main/java/com/example/eveant/user/registration/ActivationFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,8 +24,13 @@ import com.example.eveant.MainActivity;
 import com.example.eveant.R;
 import com.example.eveant.user.UserClientUtils;
 import com.example.eveant.user.UserService;
+import com.example.eveant.user.model.Profile;
+import com.example.eveant.user.model.User;
 
-import java.util.logging.Handler;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import android.os.Handler;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,6 +40,8 @@ import retrofit2.Retrofit;
 public class ActivationFragment extends Fragment {
 
     private Handler handler;
+    private UserService userService;
+    private String email;
     private final long checkInterval = 5000; // Check every 5 seconds
 
     @Nullable
@@ -65,8 +73,8 @@ public class ActivationFragment extends Fragment {
     private void checkActivationStatus() {
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserSession", MODE_PRIVATE);
         String token = sharedPreferences.getString("token", "");
-        
-        UserService userService = UserClientUtils.getClient().create(UserService.class);
+        fetchUserData(token);
+        userService = UserClientUtils.getClient().create(UserService.class);
         userService.checkActivationStatus(email).enqueue(new Callback<Boolean>() {
             @Override
             public void onResponse(@NonNull Call<Boolean> call, @NonNull Response<Boolean> response) {
@@ -83,6 +91,41 @@ public class ActivationFragment extends Fragment {
             @Override
             public void onFailure(@NonNull Call<Boolean> call, @NonNull Throwable t) {
                 Log.e("Activation", "Error checking activation status: " + t.getMessage());
+            }
+        });
+    }
+    private String fetchUserData(String token) {
+        String username = "";
+        try {
+            String[] parts = token.split("\\.");
+
+            if (parts.length == 3) {
+                String payload = new String(Base64.decode(parts[1], Base64.URL_SAFE), StandardCharsets.UTF_8);
+                JSONObject jsonObject = new JSONObject(payload);
+                username = jsonObject.optString("sub");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        fetchProfile(username);
+    }
+
+    private void fetchProfile(String username) {
+        Call<Profile> profileCall = userService.getProfile(username);
+        profileCall.enqueue(new Callback<Profile>() {
+            @Override
+            public void onResponse(Call<Profile> call, Response<Profile> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Profile profile = response.body();
+                    email = profile.getEmail();
+                } else {
+                    Log.e("AccountFragment2", "Failed to fetch profile");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Profile> call, Throwable t) {
+                Log.e("AccountFragment2", "Failed to fetch profile", t);
             }
         });
     }
